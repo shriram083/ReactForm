9a5c3a21eeb3768ee47f5352f5a18f3a3b86719b
Shriram <shriramdeshpande77@gmail.com>
2022-05-29 08:15:18 +0530

    form using react

---
+++ src/components/Form.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Form = () => {
+  return (
+    <div>
+        <h1>React Form</h1>
+        <div>
+            <label>Name</label>
+            <input 
+                type="text"
+                placeholder='Enter Name'
+            />
+        </div>
+    </div>
+  )
+}
+
+export default Form
